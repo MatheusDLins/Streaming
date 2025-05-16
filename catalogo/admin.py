7e658ee659f80ef38cc37d4fddfb093a9dabb10c
MatheusDLins <matheus934qwer@gmail.com>
2025-05-16 18:57:27 -0300

    adm created and serialize created

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Conteudo, Categoria
 
-# Register your models here.
+@admin.register(Conteudo)
+class ConteudoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'tipo', 'categoria', 'data_lancamento', 'disponivel')
+    list_filter = ('tipo', 'categoria')
+    search_fields = ('titulo',)
+
+admin.site.register(Categoria)
---
+++ catalogo/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Conteudo, Categoria
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+class ConteudoSerializer(serializers.modelSerializer):
+    categoria = CategoriaSerializer()
+
+    class Meta:
+        model = Conteudo
+        fields = '__all__'
