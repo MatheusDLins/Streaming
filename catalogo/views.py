fe0634216d22ed546a8751f15b960ebac9ad0f2e
MatheusDLins <matheus934qwer@gmail.com>
2025-05-16 19:02:18 -0300

    serializer finished

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Categoria, Conteudo
+from serializers import CategoriaSerializer, ConteudoSerializer
+
+class ConteudoViewSet(viewsets.ModelViewSet):
+    queryset = Conteudo.objects.all()
+    serializer_class = ConteudoSerializer
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
