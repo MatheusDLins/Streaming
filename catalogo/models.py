8b9c4508747122f56dc1e11ad3bc196bcef4d117
MatheusDLins <matheus934qwer@gmail.com>
2025-05-16 18:35:31 -0300

    BD created

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return super().__str__()
+    
+class Conteudo(models.Model):
+    TIPOS = [
+        ('F', 'Filme'),
+        ('S', 'Série')
+
+    ]
+
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    tipo = models.CharField(max_length=1, choices=TIPOS)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    data_lancamento = models.DateField()
+    disponivel = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.titulo
